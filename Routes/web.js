342aa5c97f78374be7efb9010110465970b878f4
Adeem2218 <bsse091503014@gmail.com>
2021-06-16 18:42:15 +0500

    Doctors and Laboratory updated

--- Routes/web.js
+++ Routes/web.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 router.use('/', require('./web/admin'));
 router.use('/', require('./web/auth'));
 router.use('/', require('./web/pharmacist'));
-// router.use('/', require('./web/feedback'));
+router.use('/', require('./web/doctor'));
 // router.use('/', require('./web/users'));
 // router.use('/', require('./web/settings'));
 
---
+++ controller/web/admin/doctor.js
@@ -0,0 +1,59 @@
+const Doctors = require('../../../model/doctors');
+
+module.exports = {
+  get: async (req, res, next) => {
+    try {
+      const doctors = await Doctors.find({});
+      res.status(200).render('doctorsList', { doctors });
+    } catch (error) {
+      res.status(400).send({ data: { message: error } });
+    }
+  },
+  show: async (req, res, next) => {
+    res.render('doctorAdd');
+  },
+  add: async (req, res, next) => {
+    const { name, phoneNo } = req.body;
+    try {
+      const oldDoctors = await Doctors.findOne({ name: name, phoneNo: phoneNo });
+      if (oldDoctors) {
+        res.status(400).send('Doctor already exists');
+      } else {
+        const doctor = new Doctors({ name: name, phoneNo: phoneNo });
+        await doctor.save();
+        res.redirect('/admin/doctors');
+      }
+    } catch (error) {
+      res.status(400).send({ data: { message: error } });
+    }
+  },
+  edit: async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const doctor = await Doctors.findById(id);
+      res.status(200).render('doctorEdit', { doctor });
+    } catch (error) {
+      res.status(400).send({ data: { message: error } });
+    }
+  },
+  update: async (req, res, next) => {
+    const { id } = req.params;
+    const { name, phoneNo } = req.body;
+    try {
+      await Doctors.findByIdAndUpdate(id, { name: name, phoneNo: phoneNo });
+      return res.status(200).redirect('/admin/doctors');
+    } catch (error) {
+      // throw error
+      res.status(400).send({ data: { message: error } });
+    }
+  },
+  delete: async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      await Doctors.findByIdAndRemove(id);
+      res.status(200).redirect('/admin/doctors');
+    } catch (error) {
+      res.status(400).send({ data: { message: error } });
+    }
+  },
+};
