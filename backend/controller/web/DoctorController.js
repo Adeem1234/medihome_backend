ee91f7c7bd32267af478a2ae6f78af4f642f7d64
Adeem2218 <bsse09153014@student.uol.edu.pk>
2021-07-13 11:31:21 +0500

    a

--- backend/controller/web/DoctorController.js
+++ backend/controller/web/DoctorController.js
@@ -13,13 +13,13 @@ module.exports = {
     res.render('doctorAdd');
   },
   add: async (req, res, next) => {
-    const { name, phoneNo } = req.body;
+    const { name, phoneNo, specialization, diploma } = req.body;
     try {
-      const oldDoctors = await DoctorsModel.findOne({ name: name, phoneNo: phoneNo });
+      const oldDoctors = await DoctorsModel.findOne({ name: name, phoneNo: phoneNo, specialization: specialization, diploma: diploma });
       if (oldDoctors) {
         res.status(400).send('Doctor already exists');
       } else {
-        const doctor = new DoctorsModel({ name: name, phoneNo: phoneNo });
+        const doctor = new DoctorsModel({ name: name, phoneNo: phoneNo, specialization: specialization, diploma: diploma });
         await doctor.save();
         res.redirect('/admin/doctors');
       }
@@ -41,7 +41,7 @@ module.exports = {
     const { id } = req.params;
     const { name, phoneNo } = req.body;
     try {
-      await DoctorsModel.findByIdAndUpdate(id, { name: name, phoneNo: phoneNo });
+      await DoctorsModel.findByIdAndUpdate(id, { name: name, phoneNo: phoneNo, specialization: specialization, diploma: diploma });
       return res.status(200).redirect('/admin/doctors');
     } catch (error) {
       res.status(400).send({ data: { message: error } });
